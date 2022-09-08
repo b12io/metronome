f55e10899f3ad7b1112ca1580dbfa6b57fcf4318
Elaman Bazarov <elaman.bazarov98@gmail.com>
2022-09-08 16:34:26 +0000

    comment fixes

--- components/form/toggle-button/ToggleButton.test.es6.js
+++ components/form/toggle-button/ToggleButton.test.es6.js
@@ -4,27 +4,36 @@ import React from 'react'
 import { shallow } from 'enzyme'
 import ToggleButton from './ToggleButton.es6.js'
 
-it('should render label', () => {
-  const component = shallow(<ToggleButton label="My toggle button" />)
-  expect(component.find('.ds-form-control-toggle__label').at(0).text().trim()).toEqual('My toggle button')
-})
+describe('DatetimePicker component', () => {
+  let testProps, component
 
-it('should be toggled off by default', () => {
-  const component = shallow(<ToggleButton />)
-  expect(component.find('input').at(0).prop('checked')).toBeFalsy()
-})
+  beforeEach(() => {
+    testProps = {
+      label: 'My toggle button',
+      checked: false,
+      onChange: jest.fn()
+    }
+    component = shallow(<ToggleButton {...testProps} />)
+  })
 
-it('should be checked', () => {
-  const component = shallow(<ToggleButton checked/>)
-  expect(component.find('input').at(0).prop('checked')).toBeTruthy()
-})
+  it('should render label', () => {
+    expect(component.find('.ds-form-control-toggle__label').at(0).text().trim()).toEqual('My toggle button')
+  })
 
-it('should call onChange', () => {
-  const onChange = jest.fn()
-  const component = shallow(<ToggleButton onChange={onChange} />)
-  component.find('input').simulate('change', {target: { checked: true}})
-  expect(onChange.mock.calls[0][0]).toEqual(true)
-  component.find('input').simulate('change', {target: { checked: false}})
-  expect(onChange.mock.calls[1][0]).toEqual(false)
+  it('should be toggled off by default', () => {
+    expect(component.find('input').at(0).prop('checked')).toBeFalsy()
+  })
+
+  it('should be checked', () => {
+    component.setProps({ checked: true })
+    expect(component.find('input').at(0).prop('checked')).toBeTruthy()
+  })
+
+  it('should call onChange', () => {
+    component.find('input').simulate('change', {target: { checked: true}})
+    expect(testProps.onChange.mock.calls[0][0]).toEqual(true)
+    component.find('input').simulate('change', {target: { checked: false}})
+    expect(testProps.onChange.mock.calls[1][0]).toEqual(false)
+  })
 })
 
