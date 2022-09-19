989935f4a077f6bee15961829637667c36765960
Elaman Bazarov <elaman.bazarov98@gmail.com>
2022-09-19 16:10:14 +0600

    ToggleButton implemented (#53)
    * ToggleButton implemented
    
    * lint fixed
    
    * comment fixes
    
    * comment fixes

---
+++ components/form/toggle-button/ToggleButton.test.es6.js
@@ -0,0 +1,40 @@
+/* global expect */
+
+import React from 'react'
+import { shallow } from 'enzyme'
+import ToggleButton from './ToggleButton.es6.js'
+
+describe('DatetimePicker component', () => {
+  let component
+
+  const testProps = {
+    label: 'My toggle button',
+    checked: false,
+    onChange: jest.fn()
+  }
+
+  beforeEach(() => {
+    component = shallow(<ToggleButton {...testProps} />)
+  })
+
+  it('should render label', () => {
+    expect(component.find('.ds-form-control-toggle__label').at(0).text().trim()).toEqual(testProps.label)
+  })
+
+  it('should be toggled off by default', () => {
+    expect(component.find('input').at(0).prop('checked')).toBeFalsy()
+  })
+
+  it('should be checked', () => {
+    component.setProps({ checked: true })
+    expect(component.find('input').at(0).prop('checked')).toBeTruthy()
+  })
+
+  it('should call onChange', () => {
+    component.find('input').simulate('change', {target: { checked: true}})
+    expect(testProps.onChange.mock.calls[0][0]).toEqual(true)
+    component.find('input').simulate('change', {target: { checked: false}})
+    expect(testProps.onChange.mock.calls[1][0]).toEqual(false)
+  })
+})
+
