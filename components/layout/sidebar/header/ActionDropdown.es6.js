c55f0121e3c21c4b9fdb8222f6f6b4e983fd4dfc
Ivan Zhadaev <ivan.zhadaev@b12.io>
2025-06-15 09:35:56 +0000

    Updated TW animation and sidebar onHover handler

--- components/layout/sidebar/header/ActionDropdown.es6.js
+++ components/layout/sidebar/header/ActionDropdown.es6.js
@@ -5,48 +5,57 @@ import SidebarHeaderActionButton from './SidebarHeaderActionButton.es6.js'
 import classnames from 'classnames'
 
 const ActionDropdown = forwardRef(({
-    icon,
-    actionsAvailable,
-    actionsVisible,
-    setActionsVisibility,
-    quickDialog,
-    quickActions,
-    onClick,
-    dropdownMenuLabel,
-    onQuickActionSelected,
-    isLogoDropDown,
-}, ref) => (
+  icon,
+  actionsAvailable,
+  actionsVisible,
+  setActionsVisibility,
+  quickDialog,
+  quickActions,
+  onClick,
+  dropdownMenuLabel,
+  onQuickActionSelected,
+  isLogoDropDown,
+  handleHover,
+}, ref) => {
+  const hoverHandlers = handleHover ? {
+    onMouseEnter: onClick,
+    onMouseLeave: onClick,
+  } : {}
+
+  return (
     <div
-        className={classnames({
-            'ds-dropdown': actionsAvailable && icon,
-            'ds-dropdown--right': actionsAvailable && icon && !isLogoDropDown,
-            'ds-dropdown--left': isLogoDropDown,
-            'ds-dropdown--menu-visible': actionsAvailable && actionsVisible,
-            'ds-sidebar__header-action': !actionsAvailable
-        })}
-        ref={ref}
+      className={classnames({
+        'ds-dropdown': actionsAvailable && icon,
+        'ds-dropdown--right': actionsAvailable && icon && !isLogoDropDown,
+        'ds-dropdown--left': isLogoDropDown,
+        'ds-dropdown--menu-visible': actionsAvailable && actionsVisible,
+        'ds-sidebar__header-action': !actionsAvailable
+      })}
+      ref={ref}
+      {...hoverHandlers}
     >
-        {icon && (
-            <SidebarHeaderActionButton
-                dropdown={actionsAvailable}
-                icon={icon}
-                onClick={onClick}
-            />
-        )}
+      {icon && (
+        <SidebarHeaderActionButton
+          dropdown={actionsAvailable}
+          icon={icon}
+          onClick={onClick}
+        />
+      )}
 
-        {quickDialog}
+      {quickDialog}
 
-        {actionsVisible && (
-            <QuickActions
-                actions={quickActions}
-                label={dropdownMenuLabel}
-                onSelect={(idx) => {
-                    setActionsVisibility(false)
-                    onQuickActionSelected && onQuickActionSelected(idx)
-                }}
-            />
-        )}
+      {actionsVisible && (
+        <QuickActions
+          actions={quickActions}
+          label={dropdownMenuLabel}
+          onSelect={(idx) => {
+            setActionsVisibility(false)
+            onQuickActionSelected && onQuickActionSelected(idx)
+          }}
+        />
+      )}
     </div>
-))
+  )
+})
 
 export default ActionDropdown
